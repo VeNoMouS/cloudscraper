9786349c9f51a93c9a8c2ef2f9b49add825d0731
Unknown <venom@gen-x.co.nz>
2019-04-17 11:30:50 +1200

    Javascript interpreter cleanups.

--- cloudscraper/javascript_interpreter.py
+++ cloudscraper/javascript_interpreter.py
@@ -3,7 +3,6 @@ import re
 import sys
 import logging
 
-
 ##########################################################################################################################################################
 
 BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug in the script.'
@@ -11,30 +10,30 @@ BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug
 ##########################################################################################################################################################
 
 
-class JavaScript_Interrupter():
-    def __init__(self, interrupter='js2py'):
-        self.interrupter = None
-        self.loadInterrupter(interrupter)
+class JavaScript_Interpreter():
+    def __init__(self, interpreter='js2py'):
+        self.interpreter = None
+        self.loadinterpreter(interpreter)
            
     ##########################################################################################################################################################
     
-    def loadInterrupter(self, interrupter):
-        interrupters = ['js2py', 'nodejs']
+    def loadinterpreter(self, interpreter):
+        interpreters = ['js2py', 'nodejs']
         
-        if interrupter not in interrupters:
+        if interpreter not in interpreters:
             sys.tracebacklimit = None if sys.version_info[0] == 3 else 0
             raise ValueError(
-                'Unknown interrupter "{}", please select one of the following interrupters [ {} ]'.format(
-                    interrupter,
-                    ', '.join(interrupters)
+                'Unknown interpreter "{}", please select one of the following interpreters [ {} ]'.format(
+                    interpreter,
+                    ', '.join(interpreters)
                 )
             )
         
         try:
-            mod = __import__('cloudscraper.{}_interrupter'.format(interrupter), fromlist=[interrupter])
-            self.interrupter = getattr(mod, '{}_interrupter'.format(interrupter))()
+            mod = __import__('cloudscraper.interpreters.{}'.format(interpreter), fromlist=[interpreter])
+            self.interpreter = getattr(mod, '{}'.format(interpreter))()
         except (ImportError, AttributeError) as e:
-            Exception('Unable to load {} interrupter'.format(interrupter))
+            Exception('Unable to load {} interpreter'.format(interpreter))
             
     ##########################################################################################################################################################
     
@@ -74,7 +73,7 @@ class JavaScript_Interrupter():
             )
             innerHTML = innerHTML.group(2) if innerHTML else ""
             
-            return self.interrupter.solveJS(
+            return self.interpreter.solveJS(
                 re.sub(r'\s{2,}', ' ', jsEnv.format(domain=domain, innerHTML=innerHTML), flags=re.MULTILINE | re.DOTALL),
                 js
             )
