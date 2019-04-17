ccd74eaccd6ac57934e6fd1bc165df7fd832b911
pro-src <rodneyd.teal@gmail.com>
2019-04-17 02:44:24 -0500

    Refactor interpreters

--- cloudscraper/interpreters/__init__.py
+++ cloudscraper/interpreters/__init__.py
@@ -1,9 +1,19 @@
 import re
 import sys
 import logging
+import abc
+import os
 
-from os import listdir
-from os.path import isfile, join, dirname, realpath
+from io import open
+
+interpreters = dict()
+with open(os.path.join(os.path.dirname(__file__), 'interpreters.lst')) as fp:
+    interpreters.update((k, None) for k in fp.read().split('\n'))
+
+if sys.version_info >= (3, 4):
+    ABC = abc.ABC  # noqa
+else:
+    ABC = abc.ABCMeta('ABC', (), {})
 
 ##########################################################################################################################################################
 
@@ -12,37 +22,45 @@ BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug
 ##########################################################################################################################################################
 
 
-class JavaScript_Interpreter():
-    def __init__(self, interpreter):
-        self.interpreter = None
-        self.interpreters = [
-            f.replace('_interpreter.py', '') for f in listdir(join(dirname(realpath(__file__)), 'interpreters'))
-            if f.endswith('_interpreter.py') and
-            isfile(join(join(dirname(realpath(__file__)), 'interpreters'), f))
-        ]
-        self.loadInterpreter(interpreter)
+class JavaScriptInterpreter(ABC):
+    @abc.abstractmethod
+    def __init__(self, name):
+        try:
+            if interpreters[name] is not None:
+                raise ValueError('Attempted to reload interpreter: "{}"'.format(name))
 
-    ##########################################################################################################################################################
+            interpreters[name] = self  # noqa
+        except KeyError:
+            raise ValueError('Attempted to register unknown interpreter: "{}"'.format(name))
 
-    def loadInterpreter(self, interpreter):
-        if interpreter not in self.interpreters:
+    @classmethod
+    def dynamicImport(cls, name):
+        try:
+            if interpreters[name] is not None:
+                return interpreters[name]
+        except KeyError:
             sys.tracebacklimit = None if sys.version_info[0] == 3 else 0
             raise ValueError(
                 'Unknown interpreter "{}", please select one of the following interpreters [ {} ]'.format(
-                    interpreter,
-                    ', '.join(self.interpreters)
+                    name,
+                    ', '.join(interpreters.keys())
                 )
             )
 
         try:
-            interpreter = '{}_interpreter'.format(interpreter)
-            mod = __import__('cloudscraper.interpreters.{}'.format(interpreter), fromlist=[interpreter])
-            self.interpreter = getattr(mod, '{}'.format(interpreter))()
-        except (ImportError, AttributeError) as e:
-            logging.error('Unable to load {} interpreter'.format(interpreter))
+            __import__('cloudscraper.interpreters.{}'.format(name))
+        except ImportError:
+            logging.error('Unable to load {} interpreter'.format(name))
             raise
 
-    ##########################################################################################################################################################
+        if not isinstance(interpreters[name], JavaScriptInterpreter):
+            raise ValueError('{} failed to register properly'.format(name))
+
+        return interpreters[name]
+
+    @abc.abstractmethod
+    def eval(self, jsEnv, js):
+        pass
 
     def solveChallenge(self, body, domain):
         try:
@@ -54,7 +72,7 @@ class JavaScript_Interpreter():
             raise ValueError("Unable to identify Cloudflare IUAM Javascript on website. {}".format(BUG_REPORT))
 
         js = re.sub(r'\s{2,}', ' ', js, flags=re.MULTILINE | re.DOTALL).replace('\'; 121\'', '')
-        js += '\na.value;';
+        js += '\na.value;'
 
         if 'toFixed' not in js:
             raise ValueError("Error parsing Cloudflare IUAM Javascript challenge. {}".format(BUG_REPORT))
@@ -74,17 +92,17 @@ class JavaScript_Interpreter():
             """
 
             innerHTML = re.search(
-                r'<div(?: [^<>]*)? id="([^<>]*?)">([^<>]*?)<\/div>',
+                r'<div(?: [^<>]*)? id="([^<>]*?)">([^<>]*?)</div>',
                 body,
                 re.MULTILINE | re.DOTALL
             )
             innerHTML = innerHTML.group(2) if innerHTML else ""
 
-            result = self.interpreter.solveJS(
+            result = self.eval(
                 re.sub(r'\s{2,}', ' ', jsEnv.format(domain=domain, innerHTML=innerHTML), flags=re.MULTILINE | re.DOTALL),
                 js
             )
-        except:
+        except:  # noqa
             logging.error("Error extracting Cloudflare IUAM Javascript.".format(BUG_REPORT))
             raise
 
