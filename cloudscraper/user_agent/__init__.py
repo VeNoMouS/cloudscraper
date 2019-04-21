1f56f37fc6139c9a8d8ced037508d683dbe813a2
Unknown <venom@gen-x.co.nz>
2019-04-21 20:19:35 +1200

    Add in brotli decompression support.

--- cloudscraper/user_agent/__init__.py
+++ cloudscraper/user_agent/__init__.py
@@ -5,11 +5,18 @@ import logging
 
 from collections import OrderedDict
 
+##########################################################################################################################################################
+
 class User_Agent():
+    
+    ##########################################################################################################################################################
+    
     def __init__(self, *args, **kwargs):
         self.headers = None
         self.loadUserAgent(*args, **kwargs)
-
+    
+    ##########################################################################################################################################################
+    
     def loadUserAgent(self, *args, **kwargs):
         browser = kwargs.pop('browser', 'chrome')
 
@@ -19,12 +26,14 @@ class User_Agent():
         )
 
         if not user_agents.get(browser):
-            logging.error('Sorry {} User-Agent was not found.'.format(browser))
+            logging.error('Sorry "{}" browser User-Agent was not found.'.format(browser))
             raise
 
         user_agent = random.choice(user_agents.get(browser))
+        
         self.headers= user_agent.get('headers')
         self.headers['User-Agent'] = random.choice(user_agent.get('User-Agent'))
-        if not kwargs.get('allow_br', False):
+        
+        if not kwargs.get('allow_brotli', False):
             if 'br' in self.headers['Accept-Encoding']:
                 self.headers['Accept-Encoding'] = ','.join([encoding for encoding in self.headers['Accept-Encoding'].split(',') if encoding.strip() != 'br']).strip()
