8747e4ecdf2e5bc2af47687fbd5b2420709c05b1
Unknown <venom@gen-x.co.nz>
2019-04-29 22:01:32 +1200

    Addtional classifiers
    .

--- setup.py
+++ setup.py
@@ -51,6 +51,9 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
