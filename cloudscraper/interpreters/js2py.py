31cdd58ff09c11950b8dd10f18df73ab65fa48d7
Unknown <venom@gen-x.co.nz>
2019-05-04 00:26:20 +1200

    js2py issue with latest cf change.
    js2py issue with latest cf change.
    
    
    js2py issue with latest cf change.

--- cloudscraper/interpreters/js2py.py
+++ cloudscraper/interpreters/js2py.py
@@ -15,16 +15,16 @@ class ChallengeInterpreter(JavaScriptInterpreter):
         super(ChallengeInterpreter, self).__init__('js2py')
 
     def eval(self, jsEnv, js):
-        if js2py.eval_js('(+(+!+[]+[+!+[]]+(!![]+[])[!+[]+!+[]+!+[]]+[!+[]+!+[]]+[+[]])+[])[+!+[]]') == '1':
-            logging.warning('WARNING - Please upgrade your js2py https://github.com/PiotrDabkowski/Js2Py, applying work around for the meantime.')
-            js = jsunfuck(js)
+        # if js2py.eval_js('(+(+!+[]+[+!+[]]+(!![]+[])[!+[]+!+[]+!+[]]+[!+[]+!+[]]+[+[]])+[])[+!+[]]') == '1':
+        #    logging.warning('WARNING - Please upgrade your js2py https://github.com/PiotrDabkowski/Js2Py, applying work around for the meantime.')
+        #    js = jsunfuck(js)
 
         def atob(s):
             return base64.b64decode('{}'.format(s)).decode('utf-8')
 
         js2py.disable_pyimport()
         context = js2py.EvalJs({'atob': atob})
-        result = context.eval('{}{}'.format(jsEnv, js))
+        result = context.eval('{}{}'.format(jsEnv, jsunfuck(js)))
 
         return result
 
