85a6c012879ab2dfdb0932b7bb0a2bb30a90d532
Unknown <venom@gen-x.co.nz>
2019-05-04 12:23:07 +1200

    Release 1.1.5 - my bad

--- cloudscraper/__init__.py
+++ cloudscraper/__init__.py
@@ -33,7 +33,7 @@ except ImportError:
 
 ##########################################################################################################################################################
 
-__version__ = '1.1.4'
+__version__ = '1.1.5'
 
 BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug in the script.'
 
