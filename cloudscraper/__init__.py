6f25ca0215b124c45ee11abe5232c00b5d844712
Unknown <venom@gen-x.co.nz>
2019-05-08 15:00:58 +1200

    Release 1.1.7 Make sure openssl is there.

--- cloudscraper/__init__.py
+++ cloudscraper/__init__.py
@@ -33,7 +33,7 @@ except ImportError:
 
 ##########################################################################################################################################################
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug in the script.'
 
