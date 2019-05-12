98a8d6748d0f84e6e88b1f146f5f16d20eb98f4b
Unknown <venom@gen-x.co.nz>
2019-05-12 23:17:31 +1200

    Release 1.1.10 Chakra javascript interpreter support added.
    Release 1.1.10 Chakra javascript interpreter support added.
    
    
    Chakra -> CharkaCore
    
    
    Clean up ChakraCore
    
    
    URL Fix.
    
    
    Remove blank line
    
    
    Update readme.
    
    
    Release 1.1.10 Chakra javascript interpreter support added.
    
    
    Release 1.1.10 Chakra javascript interpreter support added.
    
    
    Chakra -> CharkaCore
    
    
    Clean up ChakraCore
    
    
    URL Fix.
    
    
    Remove blank line
    
    
    Update readme.
    
    
    Fix version.

--- cloudscraper/__init__.py
+++ cloudscraper/__init__.py
@@ -33,7 +33,7 @@ except ImportError:
 
 ##########################################################################################################################################################
 
-__version__ = '1.1.9'
+__version__ = '1.1.10'
 
 BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug in the script.'
 
