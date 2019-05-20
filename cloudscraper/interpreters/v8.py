ead8f19e8966ac5724c118612b9f6d130567cba1
Unknown <venom@gen-x.co.nz>
2019-05-20 12:25:21 +1200

    License fix.

---
+++ cloudscraper/interpreters/v8.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+import sys
+
+try:
+    import v8eval
+except ImportError:
+    sys.tracebacklimit = 0
+    raise RuntimeError('Please install the python module v8eval either via pip or download it from https://github.com/sony/v8eval')
+
+from . import JavaScriptInterpreter
+
+
+class ChallengeInterpreter(JavaScriptInterpreter):
+
+    def __init__(self):
+        super(ChallengeInterpreter, self).__init__('v8')
+
+    def eval(self, jsEnv, js):
+        try:
+            return v8eval.V8().eval('{}{}'.format(jsEnv, js))
+        except: # noqa
+            RuntimeError('We encountered an error running the V8 Engine.')
+
+
+ChallengeInterpreter()
