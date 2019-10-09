6049f10abda7faf36fb59c920ef0a1ec53cb01d0
Unknown <venom@gen-x.co.nz>
2019-10-10 11:05:56 +1300

    Release 1.2.2
    Fix reCaptcha class solveCaptcha params

--- cloudscraper/reCaptcha/__init__.py
+++ cloudscraper/reCaptcha/__init__.py
@@ -8,19 +8,19 @@ if sys.version_info >= (3, 4):
 else:
     ABC = abc.ABCMeta('ABC', (), {})
 
-##########################################################################################################################################################
-
-BUG_REPORT = 'Cloudflare may have changed their technique, or there may be a bug in the script.'
-
-##########################################################################################################################################################
+# ------------------------------------------------------------------------------- #
 
 captchaSolvers = {}
+
+# ------------------------------------------------------------------------------- #
 
 
 class reCaptcha(ABC):
     @abc.abstractmethod
     def __init__(self, name):
         captchaSolvers[name] = self
+
+    # ------------------------------------------------------------------------------- #
 
     @classmethod
     def dynamicImport(cls, name):
@@ -35,14 +35,13 @@ class reCaptcha(ABC):
 
         return captchaSolvers[name]
 
+    # ------------------------------------------------------------------------------- #
+
     @abc.abstractmethod
     def getCaptchaAnswer(self, site_url, site_key, reCaptchaParams):
         pass
 
-    def solveCaptcha(self, ret, reCaptchaParams):
-        try:
-            site_key = re.search('data-sitekey="(.+?)"', ret.text).group(1)
-        except Exception as e:
-            raise ValueError("Unable to parse Cloudflare\'s reCaptcha variable 'data-sitekey': {} {}".format(e.message, BUG_REPORT))
+    # ------------------------------------------------------------------------------- #
 
-        return self.getCaptchaAnswer(ret.url, site_key, reCaptchaParams)
+    def solveCaptcha(self, site_url, site_key, reCaptchaParams):
+        return self.getCaptchaAnswer(site_url, site_key, reCaptchaParams)
