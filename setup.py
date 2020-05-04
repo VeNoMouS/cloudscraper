2773ff2c30fe18766c682ffacb23b678fb58688e
Unknown <venom@gen-x.co.nz>
2020-05-04 12:36:04 +1200

    Add pyparsing to setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires = [
         'requests >= 2.9.2',
         'requests_toolbelt >= 0.9.1',
+        'pyparsing >= 2.4.7'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
