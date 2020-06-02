312180a424789507e3d134c27bf483d0f18eacd8
C W <something@fake-url.com>
2020-06-02 00:53:16 -0700

    Allow replacing actual call to perform HTTP request via subclassing (#230)
    * Allow replacing actual call to perform HTTP request via subclassing and overriding.
    * Fix lint.

--- cloudscraper/__init__.py
+++ cloudscraper/__init__.py
@@ -158,6 +158,13 @@ class CloudScraper(Session):
         return self.__dict__
 
     # ------------------------------------------------------------------------------- #
+    # Allow replacing actual web request call via subclassing
+    # ------------------------------------------------------------------------------- #
+
+    def perform_request(self, method, url, *args, **kwargs):
+        return super(CloudScraper, self).request(method, url, *args, **kwargs)
+
+    # ------------------------------------------------------------------------------- #
     # Raise an Exception with no stacktrace and reset depth counter.
     # ------------------------------------------------------------------------------- #
 
@@ -236,7 +243,7 @@ class CloudScraper(Session):
         # ------------------------------------------------------------------------------- #
 
         response = self.decodeBrotli(
-            super(CloudScraper, self).request(method, url, *args, **kwargs)
+            self.perform_request(method, url, *args, **kwargs)
         )
 
         # ------------------------------------------------------------------------------- #
@@ -517,7 +524,7 @@ class CloudScraper(Session):
             # ------------------------------------------------------------------------------- #
 
             resp = self.decodeBrotli(
-                super(CloudScraper, self).request(resp.request.method, resp.url, **kwargs)
+                self.perform_request(resp.request.method, resp.url, **kwargs)
             )
 
             if not self.is_reCaptcha_Challenge(resp):
