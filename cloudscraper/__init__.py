b130c738950f6ebcdd824b5d37767ccc8e449853
Marek Šuppa <mrshu@users.noreply.github.com>
2022-01-29 17:07:24 +0100

    fix: Add missing comma in `get_tokens`
    * This comma has been most probably been left out unintentionally, leading to string concatenation between the two consecutive lines. This issue has been found automatically using a regular expression.

--- cloudscraper/__init__.py
+++ cloudscraper/__init__.py
@@ -782,7 +782,7 @@ class CloudScraper(Session):
                     'doubleDown',
                     'captcha',
                     'interpreter',
-                    'source_address'
+                    'source_address',
                     'requestPreHook',
                     'requestPostHook'
                 ] if field in kwargs
